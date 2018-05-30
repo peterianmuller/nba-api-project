170f48253d6c1ca37031d9bfe202e411aaead741
peterianmuller <peterianmuller@gmail.com>
2018-05-30 11:48:27 -0700

    Create new method to add text node with 'N/A' to td's that don't have data. Also update css spacing and borders

--- public/index.js
+++ public/index.js
@@ -100,6 +100,105 @@ const makeTableOfSinglePlayer = player => {
 	$('.data-container').append(table);
 };
 
+
+const makeTableOfSinglePlayerAllowAllFields = player => {
+	let table = $('<table></table>');
+
+	// create caption
+	let caption = $('<caption></caption>');
+	caption.append(document.createTextNode(`${player.firstName} ${player.lastName}`));
+	table.append(caption);
+
+	// create head
+	let thead = $('<thead></thead>');
+
+	// create tr
+	let trHead = $('<tr></tr>');
+
+	// create position header
+
+	var thPosition = $('<th></th>').append(document.createTextNode('position'));
+
+	// create height header
+	var thHeight = $('<th></th>').append(document.createTextNode('height'));
+	
+	// create jerseyNum header
+	var thJerseyNum = $('<th></th>').append(document.createTextNode('number'));
+
+	// create DOB header
+ 	
+	var thDOB = $('<th></th>').append(document.createTextNode('DOB'))
+	
+	// append trHead and tHead to table
+	
+	trHead.append(thPosition);
+	trHead.append(thHeight);
+	trHead.append(thJerseyNum);
+	trHead.append(thDOB);
+	thead.append(trHead);
+	table.append(thead);
+
+	// create table body
+	let tbody = $('<tbody></tbody>');
+	let trBody = $('<tr></tr>');
+
+
+	// append data for each table cell
+
+	// append position data
+	if (player.pos) {
+		var tdPosition = $('<td></td>').append(document.createTextNode(player.pos));
+	} else {
+		var tdPosition = $('<td></td>').append(document.createTextNode(`N/A`));
+	}
+
+
+	// append height data
+	if (player.heightFeet !== '-') {
+		var tdHeight = $('<td></td>').append(
+				document.createTextNode(`${player.heightFeet} feet`));
+		tdHeight.append('<br/>');
+		tdHeight.append(
+				document.createTextNode(`${player.heightInches} inches`));
+	} else {
+			var tdHeight = $('<td></td>').append(
+				document.createTextNode(`N/A`)
+			);
+	}
+
+	// append jersey num
+
+	if (player.jersey) {
+		var tdJerseyNum = $('<td></td>').append(document.createTextNode(player.jersey));
+	} else {
+		var tdJerseyNum = $('<td></td>').append(document.createTextNode(`N/A`));
+	}	
+
+	// append DOB
+	if (player.dateOfBirthUTC) {
+		var tdDOB = $('<td></td>').append(document.createTextNode(player.dateOfBirthUTC));
+	} else {
+		var tdDOB = $('<td></td>').append(document.createTextNode(`N/A`));
+	}
+	
+
+	// append td to tr
+
+	trBody.append(tdPosition);	
+	trBody.append(tdHeight);
+	trBody.append(tdJerseyNum);
+	trBody.append(tdDOB);
+	
+	// append tr to tbody
+	tbody.append(trBody);
+
+	// append tbody to table
+
+	table.append(tbody);
+
+	$('.data-container').append(table);
+};
+
 $('button:first-of-type').click(e => {
 	console.log('click');
 	$.ajax({
@@ -116,7 +215,8 @@ $('button:first-of-type').click(e => {
 		success: data => {
 			console.log(data);
 			for (let i = 0; i < data.length; i++) {
-				makeTableOfSinglePlayer(data[i]);
+				//makeTableOfSinglePlayer(data[i]);
+				makeTableOfSinglePlayerAllowAllFields(data[i]);
 			}
 		}
 	});
--- readme.md
+++ readme.md
@@ -4,4 +4,6 @@ We'll first set up a basic page that has a single button that will get back data
 
 We'll then have a more advanced feature that will enable to user to request data on a certain player!
 
-Woooooooo Able to grab API data!
+Some entries on the table are lacking data. 
+Currently, not displaying data for entires
+UI affected by not displaying entries. 
